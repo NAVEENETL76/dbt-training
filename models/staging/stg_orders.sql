cf288f104ecee23d6526326a400db4219eaf12b0
Naveen Kumar <naveen.etl76@yahoo.com>
2024-06-01 13:32:28 +0000

    added report views

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,9 +1,20 @@
-select o.orderid,o.orderdate,o.shipdate,o.shipmode,
-o.ordercostprice,o.ordersellingprice,
-(o.ordersellingprice-o.ordercostprice) as profit,
-c.customername,c.segment,c.country,
-p.productname,p.category,p.subcategory,
-o.created_dt
+select 
+    o.orderid,
+    to_date(o.orderdate, 'YYYY/MM/DD') as orderdate,
+    to_date(o.shipdate, 'YYYY/MM/DD') as shipdate,
+    o.shipmode,
+    o.ordercostprice,
+    o.ordersellingprice,
+    (o.ordersellingprice-o.ordercostprice) as orderprofit,
+    c.customerid,
+    c.customername,
+    c.segment,
+    c.country,
+    p.productid,
+    p.productname,
+    p.category,
+    p.subcategory,
+    o.created_dt
  from {{ ref('raw_orders') }} o
  left join {{ ref('raw_customer') }} c on o.customerid=c.customerid
  left join {{ ref('raw_product') }} p on o.productid=p.productid
